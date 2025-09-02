ea4bafcc4f9cd66c4624408e692c675e8ce38731
Aakarsh verma <aakarshvermaav05@gmail.com>
2025-09-02 16:12:31 +0530

    login page dun

--- src/Components/login.jsx
+++ src/Components/login.jsx
@@ -2,6 +2,8 @@ import Footer from "./Footer";
 import Navbar from "./Navbar";
 import { Link } from "react-router-dom";
 import { Box } from 'lucide-react';
+import { CirclePlay } from 'lucide-react';
+
 
 
 export default function Login() {
@@ -19,7 +21,7 @@ export default function Login() {
                             name="username"
                             placeholder="Enter your email"
                             required
-                            className="text-black w-full px-4 py-2 border-2 rounded-xl focus:outline-none focus:ring-2 focus:ring-green-400"
+                            className="text-black w-full px-4 py-2 border-2 border-green-200 rounded-xl focus:outline-none focus:ring-2 focus:ring-green-400"
                         />
                     </div>
 
@@ -30,13 +32,13 @@ export default function Login() {
                             name="password"
                             placeholder="Enter your password"
                             required
-                            className="text-black w-full px-4 py-2 border-2 rounded-xl focus:outline-none focus:ring-2 focus:ring-green-400"
+                            className="text-black w-full px-4 py-2 border-2 border-green-200 rounded-xl focus:outline-none focus:ring-2 focus:ring-green-400"
                         />
                     </div>
 
 
-                    <div className="bg-green-500 w-20 rounded-2xl h-8 ml-33">
-                        <Link to="d"><button className="pl-5.5 pt-0.5 text-white font-bold">Login</button></Link>
+                    <div className="w-full pl-4.5 pt-1 text-center border-2 border-green-200 rounded-3xl text-green-500 font-bold h-10 hover:text-white hover:bg-green-600">
+                        <Link to="d"><button >Login</button></Link>
                     </div>
 
 
@@ -50,7 +52,7 @@ export default function Login() {
                         <Link><p>Forgot Password</p></Link>
                     </div>
 
-                    <div className="text-center border-2 rounded-lg h-10">
+                    <div className="text-center border-2 border-green-200 rounded-xl text-green-500 font-bold h-10 hover:text-white hover:bg-green-600">
                         <Link><button className="pt-1">Google</button></Link>
                     </div>
                 </form>
@@ -58,14 +60,20 @@ export default function Login() {
 
 
             {/* image */}
-            <div className="mt-16 bg-gradient-to-b from-green-100 to-green-300 w-[600px] min-h-screen">
+            <div className="mt-16 bg-gradient-to-b from-green-100 to-green-300 w-[600px] min-h-screen mb-0.5">
                 <div className="">
                     <h1 className="pt-7 font-bold text-green-800 text-4xl pl-15">Welcome to</h1>
                     <span className="flex justify-center-safe text-green-950 font-bold text-4xl leading-14">Virual Herbal Garden</span>
                     <p className="pt-3 text-lg pl-16 pr-10">Eplore medicinal plants in 3D, learn about their benefits, discover the connection between nature and healing.</p>
                     <img src="./assets/Images/loginImage.png" alt="image" className="h-[400px] flex justify-self-center-safe pt-5" />
-                    <h3 className="pl-16 text-xl font-medium pb-2">Interactive 3D plant models</h3>
-                    <h3 className="pl-16 text-xl font-medium pb-5">Videos & audio narrations</h3>
+                    <div className="flex gap-2">
+                        <Box className=" ml-10 mt-1"/>
+                        <h3 className=" text-xl font-medium pb-2">Interactive 3D plant models</h3>
+                    </div>
+                    <div className="flex gap-2">
+                        <CirclePlay className="ml-10 mt-1"/>
+                        <h3 className=" text-xl font-medium pb-7">Videos & audio narrations</h3>
+                    </div>
                 </div>
             </div>
         </div>
